1c83c961a8d1cbf9cd2748115303eaafb5c8d319
Sakthivel S <40761420+sakthi-sv@users.noreply.github.com>
2020-06-09 19:02:05 +0530

    UI

---
+++ src/component/Home/section/section.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './section.css';
+import {Hoc} from '../../../hoc/hoc';
+
+export const Section =props=>{
+
+    return(<Hoc>
+
+<div class="section-container">
+            <div class="section-sub-container">
+            <div class="section-paragraph-heading">
+            <h1>Whatever your style, Travel Your Way</h1>
+            </div> 
+            <div class="section-paragraph">
+            <p>Physics (from Ancient Greek: φυσική (ἐπιστήμη), romanized: physikḗ (epistḗmē), lit. 'knowledge of nature', from φύσις phýsis 'nature')[1][2][3] is the natural science that studies matter,[4] its motion and behavior through space and time, and the related entities of 
+                energy and force.[5] Physics is one of the most fundamental scientific disciplines, and its main goal is to understand how 
+                the universe behaves.</p>
+                <p>Physics is one of the oldest academic disciplines and, through its inclusion of astronomy, perhaps the oldest.[9] Over much of the past two millennia, physics, chemistry, biology, and 
+                    certain branches of mathematics were a part of natural philosophy, but during the Scientific Revolution in the 17th 
+                    century these natural sciences emerged as unique research endeavors in their own right.[b] Physics intersects with many 
+                    interdisciplinary areas of research, such as biophysics and quantum chemistry, and the boundaries of physics are not rigidly 
+                    defined. New ideas in physics often explain the fundamental mechanisms studied by other sciences[6] and suggest new avenues of 
+                    research in academic disciplines such as mathematics and philosophy.
+                </p>
+                <p>Physics is one of the oldest academic disciplines and, through its inclusion of astronomy, perhaps the oldest.[9] Over much of the past two millennia, physics, chemistry, biology, and 
+                    certain branches of mathematics were a part of natural philosophy, but during the Scientific Revolution in the 17th 
+                    century these natural sciences emerged as unique research endeavors in their own right.
+                </p>
+                <p>But this is completely erroneous, and our view may be corroborated by actual observation more effectively than by any sort 
+                    of verbal argument. For if you let fall from the same height two weights of which one is many times as heavy as the other, 
+                    you will see that the ratio of the times required for the motion does not depend on the ratio of the weights, but that the 
+                    difference in time is a very small one. And so, if the difference in the weights is not considerable, that is, of one is,
+                     let us say, double the other, there will be no difference, or else an imperceptible difference,
+                     in time, though the difference in weight is by no means negligible, with one body weighing twice as much as the other[18]
+                </p>
+              
+                <p>Physics is one of the oldest academic disciplines and, through its inclusion of astronomy, perhaps the oldest.[9] Over much of 
+                    the past two millennia, physics, chemistry, biology, and certain branches of mathematics were a part of natural philosophy, 
+                    but during the Scientific Revolution in the 17th century these natural sciences emerged as unique research endeavors in their 
+                    own right.[b] Physics intersects with many interdisciplinary areas of research, such as biophysics and quantum chemistry, and
+                     the boundaries of physics are not rigidly defined. New ideas in physics often explain the fundamental mechanisms
+                     studied by other sciences[6] and suggest new avenues of research in academic disciplines such as mathematics and philosophy.
+                </p>
+             
+                <p>But this is completely erroneous, and our view may be corroborated by actual observation more effectively than by any 
+                    sort of verbal argument. For if you let fall from the same height two weights of which one is many times as heavy as the other,
+                     you will see that the ratio of the times required for the motion does not depend on the ratio of the weights, but that the 
+                     difference in time is a very small one. And so, if the difference in the weights is not considerable, that is, of one is, 
+                     let us say, double the other, there will be no difference, or else an imperceptible difference, in time, 
+                    though the difference in weight is by no means negligible, with one body weighing twice as much as the other[18]
+                </p>
+                <p>Physics is one of the oldest academic disciplines and, through its inclusion of astronomy, perhaps the oldest.[9] Over much of the past two millennia, physics, chemistry, biology, and 
+                    certain branches of mathematics were a part of natural philosophy, but during the Scientific Revolution in the 17th 
+                    century these natural sciences emerged as unique research endeavors in their own right.
+                </p>
+               
+
+            </div>
+            </div> 
+        </div>
+
+
+
+    </Hoc>)
+
+
+
+}
